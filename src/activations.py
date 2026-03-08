794ee1f5f7b10bdfe85aeb8430e528f54fb5c734
Fajar2k5 <fajar2k5@gmail.com>
2026-03-09 01:01:02 +0700

    feat: softmax activations function

--- src/activations.py
+++ src/activations.py
@@ -1,5 +1,14 @@
+import math
+
+
 def linear():
     pass
 
 def relu():
     pass
+    
+# softmax
+def softmax(inputs: list[float]) -> list[float]:
+    exp_inputs = [math.exp(xi) for xi in inputs]
+    sum_exp_inputs = sum(exp_inputs)
+    return [ei / sum_exp_inputs for ei in exp_inputs]
