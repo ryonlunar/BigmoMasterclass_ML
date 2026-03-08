7cd6875503ba40840d4ecfb689e272f12790a88e
ryonlunar <adhimas.bimo@gmail.com>
2026-03-08 15:08:57 +0700

    fix : layer as abstract class

--- src/layers/layer.py
+++ src/layers/layer.py
@@ -1,12 +1,15 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-class Layer:
+class Layer(ABC):
     def __init__ (self) :
         self.input = None
         self.output = None
     
+    @abstractmethod
     def forward(self, input_data):
         pass
     
+    @abstractmethod
     def backward(self, output_error, learning_rate):
         pass
