6864481417b55130be1af66132a3489c1877c231
Fajar2k5 <fajar2k5@gmail.com>
2026-03-17 20:24:34 +0700

    feat: add Adam attributes to Layer abstract class

--- src/layers/layer.py
+++ src/layers/layer.py
@@ -3,7 +3,8 @@ from abc import ABC, abstractmethod
 from initializers import initialize_zero
 
 class Layer(ABC):
-    def __init__ (self, units, input_dim = None, activation = None, weight_initializer = initialize_zero(), l1=0.0, l2=0.0):
+    def __init__ (self, units, input_dim = None, activation = None, weight_initializer = initialize_zero(), l1=0.0, l2=0.0,
+                  use_adam=False, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.input_dim = input_dim
         self.units = units
         self.W = None
@@ -14,6 +15,10 @@ class Layer(ABC):
         self.weight_initializer = weight_initializer
         self.l1 = l1
         self.l2 = l2
+        self.use_adam = use_adam
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
 
     def build(self, input_dim):
         self.input_dim = input_dim
